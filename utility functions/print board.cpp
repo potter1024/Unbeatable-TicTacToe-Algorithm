e7d7c9be52f93e325ed7483724e44dcea5bb43d0
Rishab Kumar <33695234+potter1024@users.noreply.github.com>
2019-09-07 04:25:19 +0530

    Create print board.cpp

---
+++ utility functions/print board.cpp
@@ -0,0 +1,12 @@
+void print(board b)
+{
+    for(int i=0;i<3;i++)
+    {
+        for(int j=0;j<3;j++)
+        {
+            cout<<b.arr[i][j]<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+}
